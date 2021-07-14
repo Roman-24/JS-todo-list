c17271f7a40d71fe4e8f4c859cbb0d51ba4336fb
Roman Bitarovský <bitarovskyroman@gmail.com>
2021-07-14 17:00:45 +0200

    generating items to list

--- src/app.js
+++ src/app.js
@@ -9,5 +9,42 @@ todoButton.addEventListener("click", addTodo)
 
 // Functions
 function addTodo(event){
+
+    // Prevent form from submitting
     event.preventDefault()
+
+    /*
+    we need make this:
+    <div class="todo">
+        <li></li>
+        <button></button>
+        <button></button>
+    </div>
+    */
+   
+
+    // Todo DIV
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add("todo")
+
+    // Creat LI
+    const newTodo = document.createElement('li')
+    newTodo.innerText = "hey"
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+    // chceck mark button
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton)
+
+    // chceck trash button
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('complete-btn')
+    todoDiv.appendChild(trashButton)
+
+    // append to list
+    todoList.appendChild(todoDiv)
 }
